30191182dc2f172f983ed0a6926aea59101438f3
andresmontoyab <and.montoya18@gmail.com>
2018-06-05 06:55:12 -0500

    Lambda Expression Sections Challenges

--- LamdaExpression/src/main/java/LambdaSectionChallenge/Main.java
+++ LamdaExpression/src/main/java/LambdaSectionChallenge/Main.java
@@ -1,5 +1,12 @@
 package LambdaSectionChallenge;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -14,7 +21,67 @@ public class Main {
         };
 
         // Challenge Number Two
+        Function<String, String> lambda = (String source) -> {
+            StringBuilder returnVal = new StringBuilder();
+            for (int i = 0; i< source.length(); i ++) {
+                if (i % 2 == 1) {
+                    returnVal.append(source.charAt(i));
+                }
+            }
+            return returnVal.toString();
+        };
 
-        
+        // CHallenge three and five
+        String challengeTwo = everySecondChar( lambda, "1234567890");
+        System.out.println(challengeTwo);
+
+        // Challenge six
+        Supplier<String> iLoveJava = () -> "I Love Java!";
+
+        // Challenge seve
+        String supplierResult = iLoveJava.get();
+        System.out.println(supplierResult);
+
+        //Challenge Nine and ten
+        List<String> topNames2015 = Arrays.asList(
+                "Amelia",
+                "Olivia",
+                "emily",
+                "Isla",
+                "Ava",
+                "oliver",
+                "Jack",
+                "Charlie",
+                "harry",
+                "Jacob");
+
+        topNames2015.stream()
+                .map(names -> names.substring(0,1).toUpperCase() + names.substring(1, names.length()))
+                .sorted()
+                .forEach(System.out::println);
+
+        // Challenge Twelve
+
+        long namesTimeA = topNames2015.stream()
+                .map(names -> names.substring(0,1).toUpperCase() + names.substring(1, names.length()))
+                .filter(names -> names.startsWith("A"))
+                .sorted()
+                .count();
+
+        System.out.println(namesTimeA);
+
+
+        topNames2015.stream()
+                .map(names -> names.substring(0,1).toUpperCase() + names.substring(1, names.length()))
+                .peek(System.out::println)
+                .sorted()
+        ;
+
+
+    }
+
+    // CHallenge four
+    public static String everySecondChar(Function<String, String> operation , String source){
+        return operation.apply(source);
     }
 }
